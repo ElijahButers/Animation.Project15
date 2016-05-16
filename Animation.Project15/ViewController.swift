d90fea89bb911452b53b7ecd6da0707adae4689b
ElijahButers <Elijahbuters@gmail.com>
2016-05-16 23:13:42 +0300

    Initial Commit

---
+++ Animation.Project15/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animation.Project15
+//
+//  Created by User on 5/16/16.
+//  Copyright © 2016 Elijah Buters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
