622b9ddaea9b810f7106cb5cf6e0b2f672086826
ElijahButers <Elijahbuters@gmail.com>
2016-05-17 14:20:09 +0300

    Added code to tapped() method that scales it back down (resetting to defaults), then do nothing for lots of taps, then repeats the scale up/scale down.

--- Animation.Project15/ViewController.swift
+++ Animation.Project15/ViewController.swift
@@ -35,6 +35,8 @@ class ViewController: UIViewController {
             switch self.currentAnimation {
             case 0:
                 self.imageView.transform = CGAffineTransformMakeScale(2, 2)
+            case 1:
+                self.imageView.transform = CGAffineTransformIdentity
             default:
                 break
             }
